56ddd72df45b4a39f1281afa5627fb8df10f627f
Guilherme Pereira <guilherme.expansiva@gmail.com>
2026-08-05 18:42:21 -0300

    Update pluginExploreList.ts

--- l2/pluginExplore/pluginExploreList.ts
+++ l2/pluginExplore/pluginExploreList.ts
@@ -962,8 +962,8 @@ export class PluginExploreList extends PluginBaseModule {
     private fireEventLoadProject(): void {
         if (this.fireEventThisProject === mls.actualProject) return;
         this.fireEventThisProject = mls.actualProject as number;
-        readProjectTypescriptAndCompile(mls.actualProject as number, '', true);
-        readProjectTypescriptAndCompileL1(mls.actualProject as number, '', true);
+        readProjectTypescriptAndCompile(mls.actualProject as number, '', false);    
+        //readProjectTypescriptAndCompileL1(mls.actualProject as number, '', true);
     }
 
     private changeListTimeout: number = 0;
